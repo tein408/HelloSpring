eb2e8cc3c5aefac4ebc17eb8ddfb4e01b6c17151
aria22408 <tein408@gmail.com>
2020-12-26 23:32:50 +0900

    Initial commit

---
+++ src/main/java/controller/HelloController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+public class HelloController {
+
+    @RequestMapping("/hello.do")
+    public ModelAndView printHello(){
+
+        String data = "hello world~!!!!";
+
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("data",data);
+        mav.setViewName("HelloPrint");
+
+        return mav;
+
+    }//ModelAndView
+
+}//class
---
+++ web/WEB-INF/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: tein408
+  Date: 2020-12-26 0026
+  Time: 오후 9:28
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
+<html>
+<head>
+    <title>$Title$</title>
+</head>
+<body>
+    <a href="HelloPrint.do">hihihi</a>
+</body>
+</html>
