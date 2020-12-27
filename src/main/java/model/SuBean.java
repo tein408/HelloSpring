ba13a77d62fb4b099ac7e1cea579f8cd73c20969
aria22408 <tein408@gmail.com>
2020-12-27 16:22:11 +0900

    SuBean 추가

---
+++ src/main/java/model/SuBean.java
@@ -0,0 +1,79 @@
+package model;
+
+import java.sql.Date;
+
+public class SuBean {
+
+    private int no;
+    private String name;
+    private String info;
+    private String img;
+    private int price;
+    private Date date;
+    private String temp;
+    private String cate;
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTemp() {
+        return temp;
+    }
+
+    public void setTemp(String temp) {
+        this.temp = temp;
+    }
+
+    public String getCate() {
+        return cate;
+    }
+
+    public void setCate(String cate) {
+        this.cate = cate;
+    }
+}
