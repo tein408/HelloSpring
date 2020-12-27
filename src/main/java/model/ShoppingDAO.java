9cb978119c64df9f5873dfd126b1bae57da892ac
aria22408 <tein408@gmail.com>
2020-12-27 16:23:56 +0900

    shoppingDAO 추가

---
+++ src/main/java/model/ShoppingDAO.java
@@ -0,0 +1,23 @@
+package model;
+
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+public class ShoppingDAO {
+
+    JdbcTemplate template;
+    DataSource dataSource;
+
+    public void setDataSource(DataSource dataSource){
+        this.dataSource=dataSource;
+        template = new JdbcTemplate(dataSource);
+    }//setDataSource
+
+    public List<SuBean> getAllSutool() {
+        return null;
+    }//getAllSutool
+
+}//class
