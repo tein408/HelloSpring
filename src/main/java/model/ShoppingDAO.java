7570f4b4dc65a234b34004d498b6d29c1c11d03c
aria22408 <tein408@gmail.com>
2020-12-29 19:39:56 +0900

    dao 추가

--- src/main/java/model/ShoppingDAO.java
+++ src/main/java/model/ShoppingDAO.java
@@ -2,11 +2,12 @@ package model;
 
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.util.List;
 
-
+@Repository
 public class ShoppingDAO {
 
     JdbcTemplate template;
