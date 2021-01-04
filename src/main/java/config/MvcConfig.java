1aa363b836d103b25aee2895296932c1d8c9ebba
aria22408 <tein408@gmail.com>
2021-01-05 00:15:41 +0900

    feat:MvcConfig class 추가

---
+++ src/main/java/config/MvcConfig.java
@@ -0,0 +1,24 @@
+package config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+
+
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/view/",".jsp");
+    }
+
+
+}
