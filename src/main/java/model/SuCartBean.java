9a155cc3fbaff411e0843c479c21acad65fc8412
aria22408 <tein408@gmail.com>
2021-01-06 10:49:49 +0900

    feat:카트에 담을 정보 저장하는 빈 클래스 작성

--- src/main/java/model/SuCartBean.java
+++ src/main/java/model/SuCartBean.java
@@ -1,6 +1,49 @@
 package model;
 
 public class SuCartBean {
+    private int no;
+    private String name;
+    private String img;
+    private String optid;
+    private int price;
 
+    public int getNo() {
+        return no;
+    }
 
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getOptid() {
+        return optid;
+    }
+
+    public void setOptid(String optid) {
+        this.optid = optid;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }//class
