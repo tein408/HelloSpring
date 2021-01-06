8eab68c9817fc98227afba971d483b526594c784
aria22408 <tein408@gmail.com>
2021-01-06 14:06:43 +0900

    feat:장바구니에 제품 담는 기능 추가

--- src/main/java/controller/Cart.java
+++ src/main/java/controller/Cart.java
@@ -1,5 +1,30 @@
 package controller;
 
+import model.SuCartBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
+
+    private List<SuCartBean> itemlist = new ArrayList<SuCartBean>();
+
+    public List<SuCartBean> getItemlist(){
+        return itemlist;
+    }
+
+    public void push(SuCartBean cartBean){
+        boolean alreadysutool = false;
+        for(SuCartBean suCartBean : itemlist){
+            if(cartBean.getNo() == suCartBean.getNo()){
+                suCartBean.setOptid(suCartBean.getOptid()+cartBean.getOptid());
+                alreadysutool=true;
+                break;
+            }
+        }//for
+        if(!alreadysutool){
+            itemlist.add(cartBean);
+        }
+    }//push
 
 }//class
