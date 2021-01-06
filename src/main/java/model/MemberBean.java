9bc3a6145ffd3071a4203081735233c5c6776c07
aria22408 <tein408@gmail.com>
2021-01-06 16:04:46 +0900

    feat:MemberBean 작성

--- src/main/java/model/MemberBean.java
+++ src/main/java/model/MemberBean.java
@@ -1,7 +1,73 @@
 package model;
 
+import java.sql.Date;
+
 public class MemberBean {
 
+    private String id;
+    private String pwd;
+    private String pwd2;
+    private String name;
+    private String phone;
+    private Date regdate;
 
+    @Override
+    public String toString() {
+        return "MemberBean{" +
+                "id='" + id + '\'' +
+                ", pwd='" + pwd + '\'' +
+                ", pwd2='" + pwd2 + '\'' +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", regdate=" + regdate +
+                '}';
+    }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getPwd2() {
+        return pwd2;
+    }
+
+    public void setPwd2(String pwd2) {
+        this.pwd2 = pwd2;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getRegdate() {
+        return regdate;
+    }
+
+    public void setRegdate(Date regdate) {
+        this.regdate = regdate;
+    }
 }//class
