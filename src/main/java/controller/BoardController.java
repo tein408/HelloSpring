4ec5686e5ca32e8c7e1720223cf7e593a51b0794
aria22408 <tein408@gmail.com>
2021-01-08 19:26:22 +0900

    feat:매핑 추가

--- src/main/java/controller/BoardController.java
+++ src/main/java/controller/BoardController.java
@@ -1,5 +1,12 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board/*")
 public class BoardController {
 
-}
+
+
+}//class
