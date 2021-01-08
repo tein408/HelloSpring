d0dea992c37fcb859dbe70d183105dab21037a27
aria22408 <tein408@gmail.com>
2021-01-08 20:03:46 +0900

    test:BoardDAO#getAllBoart() 테스트

--- src/test/java/Test/BoardTest.java
+++ src/test/java/Test/BoardTest.java
@@ -1,4 +1,33 @@
 package Test;
 
+import config.ShoppingDaoConfig;
+import model.BoardDAO;
+import model.ShoppingDAO;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class BoardTest {
+
+    private Logger logger = Logger.getLogger(BoardDAO.class);
+
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/shopping";
+    private static final String USER = "shopping";
+    private static final String PW = "1234";
+
+    BoardDAO boardDAO;
+
+    @Test
+    public void test2() {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ShoppingDaoConfig.class);
+        boardDAO = ctx.getBean(BoardDAO.class);
+        try {
+            logger.debug(boardDAO.getAllBoard().get(0).toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ctx.close();
+    }//test2()
+
 }
