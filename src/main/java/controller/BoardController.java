3e5094a41c4573b04097060c7d40e1fec07e0b5f
aria22408 <tein408@gmail.com>
2021-01-08 19:36:30 +0900

    feat:BoardController, BoardService 연결

--- src/main/java/controller/BoardController.java
+++ src/main/java/controller/BoardController.java
@@ -2,10 +2,19 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import service.BoardService;
 
 @Controller
 @RequestMapping("/board/*")
 public class BoardController {
+
+    BoardService boardService;
+
+    public void boardService(BoardService boardService){
+        this.boardService = boardService;
+    }
+
+
 
 
 
