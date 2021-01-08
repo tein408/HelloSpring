77286356871ba7d8be8c753e7ad21543e3e89b1e
aria22408 <tein408@gmail.com>
2021-01-08 19:16:45 +0900

    feat:BoardDAO와 JdbdTemplate 연결

--- src/main/java/model/BoardDAO.java
+++ src/main/java/model/BoardDAO.java
@@ -1,5 +1,16 @@
 package model;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class BoardDAO {
 
-}
+    JdbcTemplate jdbcTemplate;
+
+    public BoardDAO(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+
+
+}//class
