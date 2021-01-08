ea8fac9f485bc6b5c7b84c11fc4c9087a1090096
aria22408 <tein408@gmail.com>
2021-01-08 20:56:17 +0900

    feat:게시글 전체 조회 구현

--- src/main/java/model/BoardDAO.java
+++ src/main/java/model/BoardDAO.java
@@ -2,6 +2,11 @@ package model;
 
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 public class BoardDAO {
 
@@ -11,6 +16,21 @@ public class BoardDAO {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-
+    public List<BoardBean> getAllBoard() {
+        List<BoardBean> list = jdbcTemplate.query("select * from board",
+                new RowMapper<BoardBean>() {
+                    @Override
+                    public BoardBean mapRow(ResultSet resultSet, int i) throws SQLException {
+                        BoardBean boardBean = new BoardBean(
+                                resultSet.getInt("num"),resultSet.getString("writer"),
+                                resultSet.getString("email"),resultSet.getString("subject"),
+                                resultSet.getString("pwd"),resultSet.getDate("regdate"),
+                                resultSet.getInt("readcount"),resultSet.getString("content")
+                        );
+                        return boardBean;
+                    }
+                });
+        return list;
+    }//getAllBoard()
 
 }//class
