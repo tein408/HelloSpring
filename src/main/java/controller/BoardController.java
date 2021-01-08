5b5a3e92e916be273a76649018f896d98c91d6ab
aria22408 <tein408@gmail.com>
2021-01-08 21:15:06 +0900

    feat:고객게시판 게시글 전체 조회

--- src/main/java/controller/BoardController.java
+++ src/main/java/controller/BoardController.java
@@ -1,8 +1,14 @@
 package controller;
 
+import model.BoardBean;
+import model.SuBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import service.BoardService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/board/*")
@@ -10,9 +16,22 @@ public class BoardController {
 
     BoardService boardService;
 
+    @Autowired
     public void boardService(BoardService boardService){
         this.boardService = boardService;
     }
+
+    @RequestMapping("/board.do")
+    public ModelAndView getAllBoard(){
+        ModelAndView mav = new ModelAndView();
+        List<BoardBean> list = boardService.getAllBoard();
+        mav.addObject("list",list);
+        mav.addObject("center","../board/boardList.jsp");
+        mav.addObject("left","../board/boardLeft.jsp");
+        mav.setViewName("/SuMenu/ShoppingMain");
+        return mav;
+    }
+
 
 
 
