53e638ea0fe3397f6ce230502a7e9f68659797b3
aria22408 <tein408@gmail.com>
2021-01-08 19:34:39 +0900

    feat:BoardDAO 연결

--- src/main/java/service/BoardService.java
+++ src/main/java/service/BoardService.java
@@ -1,7 +1,12 @@
 package service;
 
+import model.BoardDAO;
+
 public class BoardService {
 
-
+    BoardDAO boardDAO;
+    public BoardService(BoardDAO boardDAO){
+        this.boardDAO = boardDAO;
+    }
 
 }//class
